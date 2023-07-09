cdfbf67ad4f768bfdb41a7f94da3777169e55bf0
Lyndon Kwan <kwan.lyndon@gmail.com>
2023-07-08 21:22:30 -0400

    Import Audio Assets and Add in game music

---
+++ Assets/Scripts/Audio/SFXManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour, IManager
+{
+    public void OnLoad()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void playupgradesfx()
+    {
+        
+    }
+}
